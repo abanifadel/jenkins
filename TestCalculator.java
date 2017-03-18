7314307351dfefdb9314e3e01865885b00c9b248
abanifadel <alaa.banifadel@gmail.com>
2017-03-18 22:29:41 +0100

    unit test

--- TestCalculator.java
+++ TestCalculator.java
@@ -5,5 +5,14 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TestCalculator {
+  
+  @Test
+  public void testAddition()  {
+    
+    Calculator calc = new Calculator();
+    int sumRes = calc.addition(5,5);
+    Assert.assertEquals(10,sumRes)
+  
+    {
 
 }
