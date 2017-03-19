2141ea62b9006d51d4fe20bb6d2ff70eb8d745d7
abanifadel <alaa.banifadel@gmail.com>
2017-03-19 18:55:58 +0100

    Update Calculator.java

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -8,13 +8,12 @@ public class Calculator
     private int solution;
     private int x;
     private int y;
-    private char operators;
-
+    
     public Calculator()
     {
         solution = 0;
-        Scanner operators = new Scanner(System.in);
-        Scanner operands = new Scanner(System.in);
+        x = 0;
+        y = 0;
     }
 
     public int addition(int x, int y)
@@ -34,33 +33,4 @@ public class Calculator
        solution = x / y;
        return solution;
     }
-    public void main (String[] args)
-    {
-      System.out.println("What operation? ('+', '-', '*', '/')"); 
-
-      System.out.println("Insert 2 numbers to be subtracted");
-       System.out.println("operand 1: ");
-       x = operands;
-       System.out.println("operand 2: ");
-       y = operands.next();
-      switch(operators)
-      {
-          case('+'):
-            addition(operands);
-            operands.next();
-            break;
-          case('-'):
-            subtraction(operands);
-            operands.next();
-            break;
-          case('*'):
-            multiplication(operands);
-            operands.next();
-            break;
-          case('/'):
-            division(operands);
-            operands.next();
-            break;
-       }
-  }
 }
