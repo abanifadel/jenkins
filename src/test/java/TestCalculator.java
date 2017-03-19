b86ef91213e38215a5b55863194a7e4120f059d8
abanifadel <alaa.banifadel@gmail.com>
2017-03-19 18:41:08 +0100

    Create TestCalculator.java

---
+++ src/test/java/TestCalculator.java
@@ -0,0 +1,18 @@
+package jenkins.demo.calc;
+
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestCalculator {
+  
+  @Test
+  public void testAddition()  {
+    
+    Calculator calc = new Calculator();
+    int sumRes = calc.addition(5,5);
+    Assert.assertEquals(10,sumRes)
+  
+    {
+
+}
